285f1e52361a89cde69896f8806273ee84989f4a
Maweheb <32761677+Maweheb@users.noreply.github.com>
2017-10-25 16:29:55 +0200

    Update Helloworld.java

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -6,7 +6,7 @@ public class Helloworld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println ("Prog github pushed");
+		System.out.println ("Prog github pulled");
 
 	}
 
